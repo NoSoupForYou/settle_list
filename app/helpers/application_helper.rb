00885b284373ae5565044f3873868df7a1b5f0de
Glenn Gentzke <glenn.gentzke@gmail.com>
2014-04-30 23:49:15 -0400

    Adds helper functions to convert seconds to human and back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  # Returns "MM:SS" from n
+  def duration_to_human secs
+    "#{secs/60}:#{'%02d'%(secs%60)}"
+  end
+
+  # Returns n from "MM:SS"
+  def duration_from_human dur
+    if dur =~ /^\d+$/
+      dur.to_i
+    elsif dur =~ /^(\d+):(\d+)$/
+      $1.to_i*60 + $2.to_i
+    else
+      0
+    end
+  end
+
 end
