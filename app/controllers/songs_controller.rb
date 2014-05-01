3acfd1d7c9433758c7ff0a05e495631edbafb002
Glenn Gentzke <glenn.gentzke@gmail.com>
2014-04-30 23:29:53 -0400

    adds ability to set artist for songs, improves the saving behavior

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,6 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
-  # GET /songs
-  # GET /songs.json
   def index
     if params[:tag]
       @songs = Song.tagged_with params[:tag]
@@ -11,28 +9,22 @@ class SongsController < ApplicationController
     end
   end
 
-  # GET /songs/1
-  # GET /songs/1.json
   def show
   end
 
-  # GET /songs/new
   def new
     @song = Song.new
   end
 
-  # GET /songs/1/edit
   def edit
   end
 
-  # POST /songs
-  # POST /songs.json
   def create
     @song = Song.new(song_params)
 
     respond_to do |format|
       if @song.save
-        format.html { redirect_to @song, notice: 'Song was successfully created.' }
+        format.html { redirect_to redirect_to songs_path, notice: 'Song was successfully created.' }
         format.json { render :show, status: :created, location: @song }
       else
         format.html { render :new }
@@ -41,12 +33,10 @@ class SongsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /songs/1
-  # PATCH/PUT /songs/1.json
   def update
     respond_to do |format|
       if @song.update(song_params)
-        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
+        format.html { redirect_to songs_path, notice: 'Song was successfully updated.' }
         format.json { render :show, status: :ok, location: @song }
       else
         format.html { render :edit }
@@ -55,8 +45,6 @@ class SongsController < ApplicationController
     end
   end
 
-  # DELETE /songs/1
-  # DELETE /songs/1.json
   def destroy
     @song.destroy
     respond_to do |format|
@@ -73,6 +61,6 @@ class SongsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
-      params.require(:song).permit(:title, :duration, :tag_list)
+      params.require(:song).permit(:artist_id, :duration, :tag_list, :title)
     end
 end
